117f07bd5decd11394a13709d320257b4073e95a
David Gu <david.gurquiza@gmail.com>
2013-09-24 22:56:47 -0500

    :bowtie: A baby is born.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eniv::Application.config.secret_key_base = '52ab6a8c957cf9112281630d216ea382e8b2d4ef4c17e798e0d0ef37bf040f9befc417369e240ed38b404c98f6002bbf760d6229007707ef1d51cb6393ca7150'
